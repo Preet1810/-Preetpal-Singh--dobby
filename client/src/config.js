33410bccc5b2bec171b4ce52ee63f485bf3a1303
Preet1810 <preetpalsingh1239@gmail.com>
2023-03-29 05:32:07 +0530

    deploy

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,7 @@
 const config={
     // API_BASE_URL: 'https://memories-server-edyz.onrender.com', //production
-    API_BASE_URL: 'http://localhost:3000', //development
+    // API_BASE_URL: 'http://localhost:3000', //development
+    API_BASE_URL: 'https://dobbyimage.onrender.com', //productions
     MAX_FILE_SIZE: 26214400
 };
 
